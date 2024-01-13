5a2862f2c6ae7c5fd341508753bc2b1056cf4a7e
MananPatel21 <21ceuos018@ddu.ac.in>
2024-01-13 16:44:38 +0530

    get doctors by type done

--- src/components/DoctorPage.jsx
+++ src/components/DoctorPage.jsx
@@ -1,15 +1,19 @@
 // DoctorPage.js
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const DoctorPage = () => {
+  const { doctorType } = useParams();
+  // const doctorType = 'doctor';
   const [doctorData, setDoctorData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('/getAllDoctors');
+        const response = await axios.get(`http://localhost:8001/doctor/getDoctorsByType/${doctorType}`);
         setDoctorData(response.data);
+        console.log(response.data[0]);
       } catch (error) {
         console.error('Error fetching doctor data:', error);
       }
@@ -21,13 +25,13 @@ const DoctorPage = () => {
   return (
     <div>
       <h2>Doctors</h2>
-      <ul>
+      {/* <ul>
         {doctorData.map((doctor) => (
           <li key={doctor.id}>{doctor.name}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
-  );
+  );  
 };
 
 export default DoctorPage;
