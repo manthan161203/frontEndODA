b634e2c9260b158275568b635879c5af04424bd0
manthan161203 <manthanlp3204@gmail.com>
2024-01-13 13:49:21 +0530

    working on Header Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import './App.css';
+import ResponsiveAppBar from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <ResponsiveAppBar />
+      {/* Your other components or routes go here */}
     </div>
   );
 }
